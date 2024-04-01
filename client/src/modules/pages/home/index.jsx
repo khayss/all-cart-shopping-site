0a24aa6c99a337cc99b264d5194f744a1fc04456
KayPrime <forendals@gmail.com>
2024-04-01 10:33:00 +0100

    use useMemo  to prevent re-rendering of random items on home page

--- client/src/modules/pages/home/index.jsx
+++ client/src/modules/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { ProductContext } from "../../contexts/productContext";
 import HomeCard from "./components/HomeCard";
 import { CircularProgress } from "@mui/material";
@@ -9,6 +9,24 @@ const LandingPage = () => {
   const { products, isFetching, isError } = useContext(ProductContext);
   const { cartDispatch } = useContext(CartDispatchContext);
   const { cartState } = useContext(CartContext);
+
+  const randomProducts = (products) => {
+    return (
+      products &&
+      Array.isArray(products) &&
+      products
+        .slice()
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 3)
+    );
+  };
+  const PopularBrands = useMemo(() => randomProducts(products), [products]);
+  const featuredProducts = useMemo(() => randomProducts(products), [products]);
+  const trendingCategories = useMemo(
+    () => randomProducts(products),
+    [products]
+  );
+
   return isFetching && products === null ? (
     <div className="w-full h-dvh flex flex-col items-center justify-center">
       <CircularProgress />
@@ -28,62 +46,45 @@ const LandingPage = () => {
       <div className="border-t-2 border-gray-200 h-auto">
         <h2 className="text-xl font-medium py-2">Featured Items</h2>
         <div className="grid grid-cols-1 gap-y-2 md:gap-y-0 md:gap-x-2 md:grid-cols-2 lg:grid-cols-3 h-auto">
-          {products &&
-            Array.isArray(products) &&
-            products
-              ?.slice(1, 4)
-              .reverse()
-              .map((product) => (
-                <HomeCard
-                  key={product.id}
-                  header={product.title}
-                  src={product.thumbnail}
-                  inCart={!!cartState.items[product.id]}
-                  to={`/product/${product.id}`}
-                  onClick={() => {
-                    cartDispatch({ type: "ADD_TO_CART", payload: product });
-                  }}
-                  withButton={true}
-                />
-              ))}
+          {featuredProducts.map((product) => (
+            <HomeCard
+              key={product.id}
+              header={product.title}
+              src={product.thumbnail}
+              inCart={!!cartState.items[product.id]}
+              to={`/product/${product.id}`}
+              onClick={() => {
+                cartDispatch({ type: "ADD_TO_CART", payload: product });
+              }}
+              withButton={true}
+            />
+          ))}
         </div>
       </div>
       <div className="border-t-2 border-gray-200 h-auto">
         <h2 className="text-xl font-medium py-2">Popular Brands</h2>
         <div className="grid grid-cols-1 gap-y-2 md:gap-y-0 md:gap-x-2 md:grid-cols-2 lg:grid-cols-3 h-auto">
-          {products &&
-            Array.isArray(products) &&
-            products
-              ?.slice()
-              .sort(() => 0.5 - Math.random())
-              .slice(0, 3)
-              .map((product) => (
-                <HomeCard
-                  key={product.id}
-                  header={product.brand}
-                  src={product.images[1]}
-                  to={""}
-                />
-              ))}
+          {PopularBrands.map((product) => (
+            <HomeCard
+              key={product.id}
+              header={product.brand}
+              src={product.images[1]}
+              to={""}
+            />
+          ))}
         </div>
       </div>
       <div className="border-t-2 border-gray-200">
         <h2 className="text-xl font-medium py-2">Featured Categories</h2>
         <div className="grid grid-cols-1 gap-y-2 md:gap-y-0 md:gap-x-2 md:grid-cols-2 lg:grid-cols-3 h-auto">
-          {products &&
-            Array.isArray(products) &&
-            products
-              .slice()
-              .sort(() => 0.5 - Math.random())
-              .slice(0, 3)
-              .map((product) => (
-                <HomeCard
-                  key={product.id}
-                  header={product.category}
-                  src={product.images[2]}
-                  to={""}
-                />
-              ))}
+          {trendingCategories.map((product) => (
+            <HomeCard
+              key={product.id}
+              header={product.category}
+              src={product.images[2]}
+              to={""}
+            />
+          ))}
         </div>
       </div>
     </div>
